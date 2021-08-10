eaabfc6149061d272cce9caae2b04aa7a69685cc
Awa Dieudonne <dieudonneawa7@gmail.com>
2021-08-10 22:31:48 +0100

    chore(storybook): configure storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import { addDecorator } from "@storybook/react";
+import { withA11y } from "@storybook/addon-a11y";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+addDecorator(withA11y);
+addDecorator((story) => story());
